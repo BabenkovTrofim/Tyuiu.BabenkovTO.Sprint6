8af5eacb0b4b3f3f62721df12428a1767fcfc6c0
BabenkovTrofim <babenkovt@bk.ru>
2025-11-26 18:41:08 +0500

    Task5

---
+++ Tyuiu.BabenkovTO.Sprint6.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BabenkovTO.Sprint6.Task5.V27.Lib
+{
+    public class DataService : ISprint6Task5V27
+    {
+        public int len = 0;
+        public double[] LoadFromDataFile(string path)
+        {
+            int index = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    len++;
+                }
+            }
+
+            double[] numsArray = new double[len];
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    numsArray[index++] = Convert.ToDouble(line);
+                }
+            }
+
+            numsArray = numsArray.Where(val => val % 5 != 0).ToArray();
+
+            return numsArray;
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint6.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BabenkovTO.Sprint6.Task5.V27.Lib;
+namespace Tyuiu.BabenkovTO.Sprint6.Task5.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine(new string[] { "C:", "Users", "Trofim", "AppData", "Local", "Temp", "InPutFileTask5V27.txt" });
+            double[] wait = {6, 3, 11, 18};
+            CollectionAssert.AreEqual(wait, ds.LoadFromDataFile(path));
+        }
+    }
+}
